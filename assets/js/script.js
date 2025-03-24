2c293f44be5c855df8328ba700cd25a9e0d8968e
mindaugas.samsonas <mindaugas.samsonas@studentas.bit.lt>
2025-03-24 11:40:30 +0200

    nope

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 const mobileNavOpen = document.querySelector('.mobile-nav__open');
+const mobileNav = document.querySelector('.header__main-nav');
 
 mobileNavOpen.addEventListener('click', () => {
     mobileNavOpen.classList.toggle('active');
-});
+    mobileNav.classList.toggle('active')});
+
